4594477050d15d9448b397b5ea399ca21b48659d
mbs9 <66876095+MBS9@users.noreply.github.com>
2024-02-05 12:28:19 +0100

    change dir + add some code

--- src/set_style.ts
+++ src/set_style.ts
@@ -1,7 +1,31 @@
-console.log("set_style.js");
+console.log("set_style.js is loaded!");
 
 const flatDom = [...document.querySelectorAll("*")] as HTMLElement[];
 
 for (const node of flatDom) {
-  node.style.backgroundColor = "";
+  // Remove all colors
+  const style = getComputedStyle(node);
+  node.style.backgroundColor = "white";
+  node.style.color = "black";
+  node.style.borderColor = "";
+  node.style.borderTopColor = "";
+  node.style.borderRightColor = "";
+  node.style.borderBottomColor = "";
+  node.style.borderLeftColor = "";
+  node.style.outlineColor = "";
+  node.style.fill = "white";
+  node.style.stroke = "white";
+  node.style.animation = "none";
+
+  // If video disable autoplay
+  if (node instanceof HTMLVideoElement) {
+    node.autoplay = false;
+  }
+
+  // remove any applied css classes
+  /*
+  while (node.classList.length > 0) {
+    node.classList.remove(node.classList.item(0));
+  }
+  */
 }
